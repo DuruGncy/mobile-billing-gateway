8875ec303fc90be5ef8b5149bbd52e7f86554ff1
MathiasGoris2440 <mathias.goris@outlook.com>
2025-11-28 23:58:38 +0100

    Downgrade

--- obj/Debug/net9.0/BillingGateway.AssemblyInfo.cs
+++ obj/Debug/net9.0/BillingGateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BillingGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c8803da428653aaaf71dfd2e387d40e307b3d9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13425bf779d6d2d307a3ead5a2ae08adc291794e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BillingGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BillingGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
